b786797f7c5886a9ddaf69d3becb60116f240b1f
milodevs <siliconvalleyn91@gmail.com>
2024-02-23 18:53:24 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+const port = 3000;
+require('dotenv').config();
+
+
+// Twilio Credentials
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const verifySid = process.env.TWILIO_VERIFY_SID;
+
+const client = require("twilio")(accountSid, authToken);
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get('/api/send-otp', (req, res) => {
+    client.verify.v2
+    .services(verifySid)
+    .verifications.create({ to: "+254793616321", channel: "sms" });
+    res.send({
+        message: "OTP Sent",
+        status: 1
+    });
+});
+
+app.post('/api/verify-otp', (req, res) => {
+    const { otp } = req.body;
+    client.verify.v2
+      .services(verifySid)
+      .verificationChecks.create({ to: "+254793616321", code: otp })
+      .then((verification_check) => {
+        console.log(verification_check.status);
+        if(verification_check.status === "approved") {
+          res.send({
+            message: "OTP Verified",
+            status: 1
+          });
+        }else{
+            res.send({
+                message: "OTP Not Verified",
+                status: 0
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.send({
+            message: error.message,
+            status: 0
+        });
+      });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
